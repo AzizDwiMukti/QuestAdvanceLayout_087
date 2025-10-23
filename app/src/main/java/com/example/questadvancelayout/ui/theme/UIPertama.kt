d82b5a9787a4b19d87f3b217fd27051eb76327d8
AzizDwiMukti <azizdwimukti@gmail.com>
2025-10-23 12:07:44 +0700

    memanggil stringResource

--- app/src/main/java/com/example/questadvancelayout/ui/theme/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout/ui/theme/UIPertama.kt
@@ -2,11 +2,13 @@ package com.example.questadvancelayout.ui.theme
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -48,10 +50,33 @@ fun Activitaspertama(modifier: Modifier){
                         contentDescription = null,
                         modifier = Modifier.size(100.dp).padding(5.dp)
                     )
+                    Spacer(modifier = Modifier.width(30.dp))
+                    Column (){
+                        Text(
+
+                        )
+                    }
+                    }
                 }
     }
-
     }
+){
 
+    )){
+        Row (){
+            Column (){
+                Text(
+                    text = stringResource(R.string.nama),
+                    fontSize = 30.dp
+
+                )
+            }
+
+        }
     }
+}
+
+
+
+
 
