182c720adba302e7cd7d937a8c8a11a3a3ba2a53
AzizDwiMukti <azizdwimukti@gmail.com>
2025-10-23 12:01:44 +0700

    memasukan gambar/logo

--- app/src/main/java/com/example/questadvancelayout/ui/theme/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout/ui/theme/UIPertama.kt
@@ -6,12 +6,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -39,6 +41,15 @@ fun Activitaspertama(modifier: Modifier){
                 containerColor = Color.DarkGrey
 
             )){
+                Row (){
+                    val gambar = painterResource(R.drawable.logo_umy)
+                    Image(
+                        painter = gambar,
+                        contentDescription = null,
+                        modifier = Modifier.size(100.dp).padding(5.dp)
+                    )
+                }
+    }
 
     }
 
