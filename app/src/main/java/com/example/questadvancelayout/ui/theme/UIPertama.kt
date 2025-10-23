45b584873428f7c108f7e022ff49e2034bd29646
AzizDwiMukti <azizdwimukti@gmail.com>
2025-10-23 11:25:00 +0700

    Initial commit

---
+++ app/src/main/java/com/example/questadvancelayout/ui/theme/UIPertama.kt
@@ -0,0 +1,8 @@
+package com.example.questadvancelayout.ui.theme
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+
+fun Activitaspertama(modifier: Modifier){
+    column(modifier = Modifier.padding())
+}
