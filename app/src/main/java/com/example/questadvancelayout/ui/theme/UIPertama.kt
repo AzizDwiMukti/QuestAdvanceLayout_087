295240794f65bc84dd5b610f34cfd3567587c233
AzizDwiMukti <azizdwimukti@gmail.com>
2025-10-23 11:50:00 +0700

    Menambahkan Fontsize

--- app/src/main/java/com/example/questadvancelayout/ui/theme/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout/ui/theme/UIPertama.kt
@@ -1,16 +1,31 @@
 package com.example.questadvancelayout.ui.theme
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
 
 fun Activitaspertama(modifier: Modifier){
     Column(modifier = Modifier.padding(top=100.dp)
         .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
+        Text(stringResource( R.String.prodi),
+            fontSize = 35.sp,
+            fontWeight = Fontweight.Bold)
+        Text(stringResource(R.String.univ),
+        fontSize = 22.sp)
+        spacer(modifier = Modifier.height(25.dp)
+         )
 
     }
