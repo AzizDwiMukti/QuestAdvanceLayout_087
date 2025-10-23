a20191b21166aadf4ff9f3861b470228dc16869a
AzizDwiMukti <azizdwimukti@gmail.com>
2025-10-23 11:34:58 +0700

    Add function AktivitasPertama untuk UI pertama

--- app/src/main/java/com/example/questadvancelayout/ui/theme/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout/ui/theme/UIPertama.kt
@@ -1,8 +1,16 @@
 package com.example.questadvancelayout.ui.theme
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 fun Activitaspertama(modifier: Modifier){
-    column(modifier = Modifier.padding())
-}
+    Column(modifier = Modifier.padding(top=100.dp)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+
+    }
