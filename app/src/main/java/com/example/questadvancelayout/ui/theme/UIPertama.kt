6b1e40078adec9f3f8029c35246b2c766b4d496a
AzizDwiMukti <azizdwimukti@gmail.com>
2025-10-23 11:57:59 +0700

    memasukan color/warna

--- app/src/main/java/com/example/questadvancelayout/ui/theme/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout/ui/theme/UIPertama.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -33,3 +34,13 @@ fun Activitaspertama(modifier: Modifier){
             .padding(12.dp)
 
     }
+    ){
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGrey
+
+            )){
+
+    }
+
+    }
+
