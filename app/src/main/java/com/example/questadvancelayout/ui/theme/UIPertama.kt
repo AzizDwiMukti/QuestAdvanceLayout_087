24ce82a4be31cf4d788e8bb3ac6001d1525f9458
AzizDwiMukti <azizdwimukti@gmail.com>
2025-10-23 11:55:32 +0700

    modifikasi spacer dan card

--- app/src/main/java/com/example/questadvancelayout/ui/theme/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout/ui/theme/UIPertama.kt
@@ -3,8 +3,10 @@ package com.example.questadvancelayout.ui.theme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -25,7 +27,9 @@ fun Activitaspertama(modifier: Modifier){
             fontWeight = Fontweight.Bold)
         Text(stringResource(R.String.univ),
         fontSize = 22.sp)
-        spacer(modifier = Modifier.height(25.dp)
-         )
+        spacer(modifier = Modifier.height(25.dp))
+        Card(modifier = Modifier
+            .fillMaxWidth(1f)
+            .padding(12.dp)
 
     }
